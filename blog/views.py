6b1e06f738f625f0cbae76c5ed06da513ad8e444
Eguki <erickolino@gmail.com>
2026-09-24 12:30:36 -0300

    Adicionando feature de formulario para adição de posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from blog.forms import PostForm
 from blog.models import Post
 
 
@@ -8,3 +9,19 @@ from blog.models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail',pk=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html',{'form':form})
